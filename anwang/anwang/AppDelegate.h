58fa32afc2c705c2795afbb415962e956ea2f6f8
Apple <apple@ananwang.local>
2016-03-13 22:05:38 +0800

    anwang

---
+++ anwang/anwang/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  anwang
+//
+//  Created by Apple on 3/13/16.
+//  Copyright © 2016 luzhouseo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
